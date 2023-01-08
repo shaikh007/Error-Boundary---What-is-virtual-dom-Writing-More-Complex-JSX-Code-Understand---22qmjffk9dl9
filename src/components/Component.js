ed1b5c63e21b3f3a08e878c0cc586db58b6c0410
Shaikh Irfan <shaikhji1998@gmail.com>
2023-01-08 18:05:51 +0530

    new Commit

--- src/components/Component.js
+++ src/components/Component.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 
 const Component = (props) => {
   const [error, setError] = useState(false);
+  props.change(setError);
+  if (error) {
+    throw new Error("Error");
+  }
 
   return <>{error && <div>{somethingNotDefined}</div>}</>;
 };
--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -3,25 +3,20 @@ import React from "react";
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = {
+      error: false,
+    };
   }
-
   static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    // You can also log the error to an error reporting service
-    logErrorToMyService(error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
       return (
         <p id="error">
-          There is an error in your react App. Please check again. This messege
-          is generated from error boundry.
+          There is an error in your react app. Please check again. This message
+          has been generated from error boundary.
         </p>
       );
     }
